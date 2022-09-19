773be3f2472641d2cbde62a5f266c922a2884551
Seva <s27@mail.ru>
2022-09-19 17:35:08 +0500

    js

---
+++ script.js
@@ -0,0 +1,40 @@
+/*Задание 1 */
+
+var str = prompt ('Введите своё имя:');
+var age = +prompt ('Введите свой возраст:');
+
+console.log(str);
+console.log(age);
+
+var example = +prompt ('125 + 25 = ?');
+var example1 = +prompt ('140 - 20 = ?');
+var example2 = +prompt ('11 * 11 = ?');
+var example3 = +prompt ('124 / 4 = ?');
+var example4 = +prompt ('15 % 5 = ?');
+console.log('125+25='+(125+25)); console.log('Ваш ответ:'+ example);
+console.log('140 - 20 ='+(140-20)); console.log('Ваш ответ:'+ example1);
+console.log('11 * 11 ='+ 11 * 11); console.log('Ваш ответ:'+ example2);
+console.log('124 / 4 ='+ 124 / 4); console.log('Ваш ответ:'+ example3);
+console.log('15 % 5 ='+15 % 5); console.log('Ваш ответ:'+ example4);
+
+
+
+/* Задание 2 */
+
+
+
+
+var X = +prompt('Введите первое число:');
+var Y = +prompt('Введите второе число:');
+var Z = +prompt('Введите третье число:');
+
+
+var res = (X + Y + Z) / 3
+
+alert(res)
+console.log(X);
+console.log(Y);
+console.log(Z);
+console.log(res);
+
+
